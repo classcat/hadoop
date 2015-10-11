fc2dfdae2b5c0183faca932f15b6f5ca41f2711f
ClassCat <support@classcat.com>
2015-10-11 14:00:37 +0900

    hbase version up

--- scripts/install_hbase.sh
+++ scripts/install_hbase.sh
@@ -14,13 +14,15 @@ mkdir -p /opt/packages
 
 cd /opt/packages
 
-wget http://ftp.riken.jp/net/apache/hbase/0.98.14/hbase-0.98.14-hadoop2-bin.tar.gz
 
-tar xfz hbase-0.98.14-hadoop2-bin.tar.gz
+wget http://ftp.riken.jp/net/apache/hbase/0.98.15/hbase-0.98.15-hadoop1-bin.tar.gz
+#wget http://ftp.riken.jp/net/apache/hbase/0.98.14/hbase-0.98.14-hadoop2-bin.tar.gz
 
-chown -R hduser.hadoop hbase-0.98.14-hadoop2
+tar xfz hbase-0.98.15-hadoop2-bin.tar.gz
 
-ln -s /opt/packages/hbase-0.98.14-hadoop2 /opt/hbase
+chown -R hduser.hadoop hbase-0.98.15-hadoop2
+
+ln -s /opt/packages/hbase-0.98.15-hadoop2 /opt/hbase
 
 echo 'export PATH=$PATH:/opt/hbase/bin' >> /home/hduser/.bash_profile
 chown hduser.hadoop /home/hduser/.bash_profile
